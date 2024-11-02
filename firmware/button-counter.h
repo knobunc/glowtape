00c8a0d32fb64a44f61b991078c34cd5a03c2a54
Henner Zeller <h.zeller@acm.org>
2024-11-02 12:28:47 -0700

    Better debouncing.

--- firmware/button-counter.h
+++ firmware/button-counter.h
@@ -2,11 +2,18 @@
 #define BUTTON_COUNTER_H
 
 #include "hardware/gpio.h"
-
 #include "pico/time.h"
 
 class ButtonCounter {
-  static constexpr int kDebounceTimeUsec = 100'000;
+  static constexpr int kDebounceTimeUsec = 50'000;
+
+  enum State {
+    kIdle,
+    kStartPress,
+    kPressed,
+    kStartRelease,
+  };
+
  public:
   ButtonCounter(int pin) : pin_(pin) {
     gpio_set_dir(pin_, GPIO_IN);
@@ -14,30 +21,58 @@ class ButtonCounter {
   }
 
   void Poll() {
-    const bool is_pressed = !gpio_get(pin_);  // Board-Button has inverted logic
     const absolute_time_t now = get_absolute_time();
-    const int64_t since_last = absolute_time_diff_us(last_press_time_, now);
-
-    if (!previous_pressed_ && is_pressed && since_last > kDebounceTimeUsec) {
-      ++count_;
-      last_press_time_ = now;
+    const bool is_pressed = !gpio_get(pin_);  // Board-Button has inverted logic
+    switch (state_) {
+      case kIdle:
+        if (is_pressed) {
+          SetState(kStartPress, now);
+        }
+        break;
+      case kStartPress:
+        if (!is_pressed) {
+          SetState(kIdle, now);  // fallback
+        } else if (StateAge(now) > kDebounceTimeUsec) {
+          ++count_;
+          SetState(kPressed, now);
+        }
+        break;
+      case kPressed:
+        if (!is_pressed) {
+          SetState(kStartRelease, now);
+        }
+        break;
+      case kStartRelease:
+        if (is_pressed) {
+          SetState(kPressed, now);  // Glitch back
+        } else if (StateAge(now) > kDebounceTimeUsec) {
+          SetState(kIdle, now);
+        }
     }
-
-    previous_pressed_ = is_pressed;
   }
 
   void Reset() {
-    previous_pressed_ = false;
+    state_ = kIdle;
     count_ = 0;
   }
 
   int count() const { return count_; }
 
  private:
+  void SetState(State s, absolute_time_t now) {
+    if (s == state_) return;
+    state_ = s;
+    state_change_time_ = now;
+  }
+
+  int64_t StateAge(absolute_time_t now) const {
+    return absolute_time_diff_us(state_change_time_, now);
+  }
+
   const int pin_;
+  State state_;
   int count_ = 0;
-  bool previous_pressed_ = false;
-  absolute_time_t last_press_time_{};
+  absolute_time_t state_change_time_{};
 };
 
 #endif
