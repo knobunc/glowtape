3d18c207702d524e63abb7c47a75506ad28309ad
Henner Zeller <h.zeller@acm.org>
2024-11-02 16:28:57 -0700

    Document shell.nix better

--- shell.nix
+++ shell.nix
@@ -1,14 +1,10 @@
-# This is a nix-shell for use with the nix package manager.
-# If you have nix installed, you may simply run `nix-shell`
-# in this repo, and have all dependencies ready in the new shell.
-
 { pkgs ? import <nixpkgs> {} }:
 let
   local-pico-sdk = pkgs.pico-sdk.override {
      withSubmodules = true;
   };
 
-  # gcode-cli is also perfect to send a line to tty and await OK
+  # gcode-cli is perfect to send a line to tty and awaiting OK
   gcode-cli = pkgs.stdenv.mkDerivation rec {
     name = "gcode-cli";
     src = pkgs.fetchFromGitHub {
@@ -21,7 +17,7 @@ let
     installPhase = "mkdir -p $out/bin; install gcode-cli $out/bin";
   };
 
-  # Generating compressed fonts from BDF
+  # Generating compressed fonts from BDF for use in embedded device.
   bdfont-data = pkgs.stdenv.mkDerivation rec {
     name = "bdfont-data";
     src = pkgs.fetchFromGitHub {
@@ -38,26 +34,27 @@ in
 pkgs.mkShell {
   buildInputs = with pkgs;
     [
+      # rp2040 toolchain
       gcc-arm-embedded
       local-pico-sdk
       picotool
       cmake python3   # build requirements for pico-sdk
 
+      # Tools needed
+      bdfont-data   # Generated embedded fonts
+      gcode-cli     # Tool to sync time from host to glowtape
+
+      # Generating casing and sync-tape
       openscad-unstable
       openscad-lsp
       prusa-slicer
       ghostscript
 
+      # EDA tooling.
       kicad
       gerbv
-      python3
       python312Packages.kicad
       zip
-      pcb2gcode
-      pstoedit
-      otf2bdf
-      gcode-cli
-      bdfont-data
     ];
   shellHook = ''
    export PICO_SDK_PATH=${local-pico-sdk}/lib/pico-sdk
